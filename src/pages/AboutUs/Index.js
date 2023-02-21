2eef5a9cf63c28490a3e2cfd6d13fb3791ceeca2
EXECUTIVEGS-6 <EXECUTIVEGS-6@fivesdigital.com>
2023-02-21 17:44:32 +0530

    added about us page

--- src/pages/AboutUs/Index.js
+++ src/pages/AboutUs/Index.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Index = () => {
   return (
-    <div>About Us</div>
+    <div>
+      <div className="flex-shrink-0 md:ml-4 xl:ml-4 2xl:ml-8">
+          <img className="h-auto w-auto md:h-80 md:w-80 lg:h-96 lg:w-96 xl:h-80 xl:w-80 2xl:h-96 2xl:w-96" src="../assets/images/cuate.png" alt="Logo" />
+        </div>
+    </div>
   )
 }
 
