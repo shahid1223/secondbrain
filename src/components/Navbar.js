7beee000ba4cf76b5abd9ff7e725ffe5cf5a9f08
shahid1223 <ms413959@gmail.com>
2023-03-19 07:13:35 +0530

    change logo img name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-            <img className="h-auto w-auto" src="/assets/iconsandlogo/logo.svg" alt="Logo" />
+            <img className="h-auto w-auto" src="/assets/iconsandlogo/Logo.svg" alt="Logo" />
           </div>
           <div className="hidden md:block">
             <div className="flex items-baseline space-x-4 ">
