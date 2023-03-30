57d50d63126192d8175d3f74e5de0964000c0575
Replit user <>
2023-03-30 05:02:31 +0000

    added login route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Fragment } from "react";
 import Navbar from "./components/Navbar";
 import AboutUsIndex from "./pages/AboutUs/Index";
-import HomeIndex from "./pages/Home/Index"
+import HomeIndex from "./pages/Home/Index";
 import BlogIndex from "./pages/Blog/Index";
+//authtentication
+import Login from "./pages/auth/Login";
+
 import {
   Route,
-  Routes
+  Routes,
+  useLocation
 } from "react-router-dom";
 import Footer from "./components/Footer";
 
 const App = () => {
-
+  let location = useLocation();
   return (
     <Fragment>
-      <Navbar/>
+      {location.pathname.split('/').pop() === 'login' ? null : <Navbar />}
       <Routes>
-        <Route path="/" element={<HomeIndex/>}/>
-        <Route path="/aboutus" element={<AboutUsIndex/>}/>
-        <Route path="/blog" element={<BlogIndex/>}/>
+        <Route path="/" element={<HomeIndex />} />
+        <Route path="/aboutus" element={<AboutUsIndex />} />
+        <Route path="/blog" element={<BlogIndex />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-      <Footer/>
-   </Fragment>
+      {location.pathname.split('/').pop() === 'login' ? null : <Footer />}
+    </Fragment>
   );
 }
 
